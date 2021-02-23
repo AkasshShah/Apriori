89b2a50b1ae134a45d3fb7e6281522b070d1bf9a
akassh <akassh.shah77@gmail.com>
2021-02-22 21:23:08 -0500

    commenting out old

--- cxx/Apriori.hpp
+++ cxx/Apriori.hpp
@@ -9,45 +9,47 @@
 
 namespace crow
 {
-    class AprioriRule
-    {
-    private:
-        std::set<std::string> antecedents;
-        std::set<std::string> consequents;
-        int support;
-        int confidence;
-    public:
-        AprioriRule(const std::set<std::string>&, const std::set<std::string>&, int, int);
-        ~AprioriRule();
-        friend std::ostream& operator<<(std::ostream& os, const AprioriRule& obj);
-    };
+    // should use ordered map implementation
+
+    // class AprioriRule
+    // {
+    // private:
+    //     std::set<std::string> antecedents;
+    //     std::set<std::string> consequents;
+    //     int support;
+    //     int confidence;
+    // public:
+    //     AprioriRule(const std::set<std::string>&, const std::set<std::string>&, int, int);
+    //     ~AprioriRule();
+    //     friend std::ostream& operator<<(std::ostream& os, const AprioriRule& obj);
+    // };
     
-    AprioriRule::AprioriRule(const std::set<std::string>& antecedents, const std::set<std::string>& consequents, int support, int confidence)
-        : antecedents(antecedents),
-        consequents(consequents),
-        support(support),
-        confidence(confidence)
-    {
-    }
+    // AprioriRule::AprioriRule(const std::set<std::string>& antecedents, const std::set<std::string>& consequents, int support, int confidence)
+    //     : antecedents(antecedents),
+    //     consequents(consequents),
+    //     support(support),
+    //     confidence(confidence)
+    // {
+    // }
 
-    AprioriRule::~AprioriRule()
-    {
-    }
+    // AprioriRule::~AprioriRule()
+    // {
+    // }
 
-    std::ostream& operator<<(std::ostream& os, const AprioriRule& obj)
-    {
-        // format:
-        // ( antecedent1, ..., antecedentn, ) -> ( consequent1, ..., consequentn, ) : support, confidence
-        os << "( ";
-        for(std::string item : obj.antecedents){
-            os << "item, ";
-        }
-        os << ") -> ( ";
-        for(std::string item : obj.consequents){
-            os << "item, ";
-        }
-        os << ") : " << obj.support << ", " << obj.confidence;
-        return(os);
-    }
+    // std::ostream& operator<<(std::ostream& os, const AprioriRule& obj)
+    // {
+    //     // format:
+    //     // ( antecedent1, ..., antecedentn, ) -> ( consequent1, ..., consequentn, ) : support, confidence
+    //     os << "( ";
+    //     for(std::string item : obj.antecedents){
+    //         os << "item, ";
+    //     }
+    //     os << ") -> ( ";
+    //     for(std::string item : obj.consequents){
+    //         os << "item, ";
+    //     }
+    //     os << ") : " << obj.support << ", " << obj.confidence;
+    //     return(os);
+    // }
     
 } // namespace crow
